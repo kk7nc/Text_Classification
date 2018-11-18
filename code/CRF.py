c77014b71187f9ed9a4bb49cb47a97099d7a6050
Kamran  Kowsari <kk7nc@virginia.edu>
2018-11-18 18:56:34 -0500

    Add files via upload

---
+++ code/CRF.py
@@ -0,0 +1,80 @@
+import nltk
+import sklearn_crfsuite
+from sklearn_crfsuite import metrics
+nltk.corpus.conll2002.fileids()
+train_sents = list(nltk.corpus.conll2002.iob_sents('esp.train'))
+test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testb'))
+def word2features(sent, i):
+    word = sent[i][0]
+    postag = sent[i][1]
+
+    features = {
+        'bias': 1.0,
+        'word.lower()': word.lower(),
+        'word[-3:]': word[-3:],
+        'word[-2:]': word[-2:],
+        'word.isupper()': word.isupper(),
+        'word.istitle()': word.istitle(),
+        'word.isdigit()': word.isdigit(),
+        'postag': postag,
+        'postag[:2]': postag[:2],
+    }
+    if i > 0:
+        word1 = sent[i-1][0]
+        postag1 = sent[i-1][1]
+        features.update({
+            '-1:word.lower()': word1.lower(),
+            '-1:word.istitle()': word1.istitle(),
+            '-1:word.isupper()': word1.isupper(),
+            '-1:postag': postag1,
+            '-1:postag[:2]': postag1[:2],
+        })
+    else:
+        features['BOS'] = True
+
+    if i < len(sent)-1:
+        word1 = sent[i+1][0]
+        postag1 = sent[i+1][1]
+        features.update({
+            '+1:word.lower()': word1.lower(),
+            '+1:word.istitle()': word1.istitle(),
+            '+1:word.isupper()': word1.isupper(),
+            '+1:postag': postag1,
+            '+1:postag[:2]': postag1[:2],
+        })
+    else:
+        features['EOS'] = True
+
+    return features
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+def sent2labels(sent):
+    return [label for token, postag, label in sent]
+
+def sent2tokens(sent):
+    return [token for token, postag, label in sent]
+
+X_train = [sent2features(s) for s in train_sents]
+y_train = [sent2labels(s) for s in train_sents]
+
+X_test = [sent2features(s) for s in test_sents]
+y_test = [sent2labels(s) for s in test_sents]
+
+
+
+crf = sklearn_crfsuite.CRF(
+    algorithm='lbfgs',
+    c1=0.1,
+    c2=0.1,
+    max_iterations=100,
+    all_possible_transitions=True
+)
+crf.fit(X_train, y_train)
+
+y_pred = crf.predict(X_test)
+print(metrics.flat_classification_report(
+    y_test, y_pred,  digits=3
+))
--- code/RCNN.py
+++ code/RCNN.py
@@ -1,13 +1,15 @@
-from keras.layers import Dropout, Dense,Input,Embedding,Flatten, AveragePooling2D, Conv2D,Reshape
-from keras.models import Sequential,Model
-from sklearn.feature_extraction.text import TfidfVectorizer
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import LSTM
+from keras.layers import Conv1D, MaxPooling1D
+from keras.datasets import imdb
+from sklearn.datasets import fetch_20newsgroups
 import numpy as np
 from sklearn import metrics
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from sklearn.datasets import fetch_20newsgroups
-from keras.layers.merge import Concatenate
-
 
 def loadData_Tokenizer(X_train, X_test,MAX_NB_WORDS=75000,MAX_SEQUENCE_LENGTH=500):
     np.random.seed(7)
@@ -26,7 +28,7 @@ def loadData_Tokenizer(X_train, X_test,MAX_NB_WORDS=75000,MAX_SEQUENCE_LENGTH=50
     X_train = text[0:len(X_train), ]
     X_test = text[len(X_train):, ]
     embeddings_index = {}
-    f = open("C:\\Users\\kamran\\Documents\\GitHub\\RMDL\\Examples\\Glove\\glove.6B.100d.txt", encoding="utf8")
+    f = open("C:\\Users\\kamran\\Documents\\GitHub\\RMDL\\Examples\\Glove\\glove.6B.50d.txt", encoding="utf8")
     for line in f:
         values = line.split()
         word = values[0]
@@ -40,19 +42,13 @@ def loadData_Tokenizer(X_train, X_test,MAX_NB_WORDS=75000,MAX_SEQUENCE_LENGTH=50
     return (X_train, X_test, word_index,embeddings_index)
 
 
+def Build_Model_RCNN_Text(word_index, embeddings_index, nclasses, MAX_SEQUENCE_LENGTH=500, EMBEDDING_DIM=50):
 
-def Build_Model_CNN_Text(word_index, embeddings_index, nclasses, MAX_SEQUENCE_LENGTH=500, EMBEDDING_DIM=100, dropout=0.5):
+    kernel_size = 2
+    filters = 256
+    pool_size = 2
+    gru_node = 256
 
-    """
-        def buildModel_CNN(word_index, embeddings_index, nclasses, MAX_SEQUENCE_LENGTH=500, EMBEDDING_DIM=50, dropout=0.5):
-        word_index in word index ,
-        embeddings_index is embeddings index, look at data_helper.py
-        nClasses is number of classes,
-        MAX_SEQUENCE_LENGTH is maximum lenght of text sequences,
-        EMBEDDING_DIM is an int value for dimention of word embedding look at data_helper.py
-    """
-
-    model = Sequential()
     embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
     for word, i in word_index.items():
         embedding_vector = embeddings_index.get(word)
@@ -66,56 +62,36 @@ def Build_Model_CNN_Text(word_index, embeddings_index, nclasses, MAX_SEQUENCE_LE
 
             embedding_matrix[i] = embedding_vector
 
-    embedding_layer = Embedding(len(word_index) + 1,
+
+
+    model = Sequential()
+    model.add(Embedding(len(word_index) + 1,
                                 EMBEDDING_DIM,
                                 weights=[embedding_matrix],
                                 input_length=MAX_SEQUENCE_LENGTH,
-                                trainable=True)
-
-    # applying a more complex convolutional approach
-    convs = []
-    filter_sizes = []
-    layer = 5
-    print("Filter  ",layer)
-    for fl in range(0,layer):
-        filter_sizes.append((fl+2,fl+2))
-
-    node = 128
-    sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-    embedded_sequences = embedding_layer(sequence_input)
-    emb = Reshape((500,10, 10), input_shape=(500,100))(embedded_sequences)
-
-    for fsz in filter_sizes:
-        l_conv = Conv2D(node, padding="same", kernel_size=fsz, activation='relu')(emb)
-        l_pool = AveragePooling2D(pool_size=(5,1), padding="same")(l_conv)
-        #l_pool = Dropout(0.25)(l_pool)
-        convs.append(l_pool)
-
-    l_merge = Concatenate(axis=1)(convs)
-    l_cov1 = Conv2D(node, (5,5), padding="same", activation='relu')(l_merge)
-    l_cov1 = AveragePooling2D(pool_size=(5,2), padding="same")(l_cov1)
-    l_cov2 = Conv2D(node, (5,5), padding="same", activation='relu')(l_cov1)
-    l_pool2 = AveragePooling2D(pool_size=(5,2), padding="same")(l_cov2)
-    l_cov2 = Dropout(dropout)(l_pool2)
-    l_flat = Flatten()(l_cov2)
-    l_dense = Dense(128, activation='relu')(l_flat)
-    l_dense = Dropout(dropout)(l_dense)
-
-    preds = Dense(nclasses, activation='softmax')(l_dense)
-    model = Model(sequence_input, preds)
+                                trainable=True))
+    model.add(Dropout(0.25))
+    model.add(Conv1D(filters, kernel_size, activation='relu'))
+    model.add(MaxPooling1D(pool_size=pool_size))
+    model.add(Conv1D(filters, kernel_size, activation='relu'))
+    model.add(MaxPooling1D(pool_size=pool_size))
+    model.add(Conv1D(filters, kernel_size, activation='relu'))
+    model.add(MaxPooling1D(pool_size=pool_size))
+    model.add(Conv1D(filters, kernel_size, activation='relu'))
+    model.add(MaxPooling1D(pool_size=pool_size))
+    model.add(LSTM(gru_node, return_sequences=True, recurrent_dropout=0.2))
+    model.add(LSTM(gru_node, return_sequences=True, recurrent_dropout=0.2))
+    model.add(LSTM(gru_node, return_sequences=True, recurrent_dropout=0.2))
+    model.add(LSTM(gru_node, recurrent_dropout=0.2))
+    model.add(Dense(1024,activation='relu'))
+    model.add(Dense(nclasses))
+    model.add(Activation('softmax'))
 
     model.compile(loss='sparse_categorical_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
 
-
-
     return model
-
-
-
-from sklearn.datasets import fetch_20newsgroups
-from RMDL import text_feature_extraction as txt
 
 newsgroups_train = fetch_20newsgroups(subset='train')
 newsgroups_test = fetch_20newsgroups(subset='test')
@@ -124,24 +100,21 @@ X_test = newsgroups_test.data
 y_train = newsgroups_train.target
 y_test = newsgroups_test.target
 
-
 X_train_Glove,X_test_Glove, word_index,embeddings_index = loadData_Tokenizer(X_train,X_test)
 
 
-model_CNN = Build_Model_CNN_Text(word_index,embeddings_index, 20)
+model_RCNN = Build_Model_RCNN_Text(word_index,embeddings_index, 20)
 
 
-model_CNN.summary()
+model_RCNN.summary()
 
-model_CNN.fit(X_train_Glove, y_train,
+model_RCNN.fit(X_train_Glove, y_train,
                               validation_data=(X_test_Glove, y_test),
-                              epochs=1000,
+                              epochs=15,
                               batch_size=128,
                               verbose=2)
 
-predicted = model_CNN.predict(X_test_Glove)
+predicted = model_RCNN.predict(X_test_Glove)
 
 predicted = np.argmax(predicted, axis=1)
-
-
 print(metrics.classification_report(y_test, predicted))
